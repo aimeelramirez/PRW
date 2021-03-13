976020496d42cf52db0556f6fcc7fb5dd700e75d
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-03-13 03:37:10 -0500

    add routes to nav

--- src/view/body/header.js
+++ src/view/body/header.js
@@ -1,43 +1,53 @@
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 
 const Header = () => {
-    return (<div>
-        <nav>
-            <ul>
-                <li>
-                    <a
-                        className="App-link"
-                        href="#"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    > Home</a>
-                </li>
-                <li>
-                    <a
-                        className="App-link"
-                        href="#"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    > Newsfeed</a>
-                </li>
-                <li>
-                    <a
-                        className="App-link"
-                        href="#"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    > Messages</a>
-                </li>
-                <li>
-                    <a
-                        className="App-link"
-                        href="#"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    > Watch</a>
-                </li>
-            </ul></nav></div>)
+    return (
+        <Router><div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/users">Users</Link>
+                    </li>
+                </ul></nav>
 
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/users">
+                    <Users />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </div></Router >)
+
+}
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Users() {
+    return <h2>Users</h2>;
 }
 export default Header;
