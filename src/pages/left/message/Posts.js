b49dbd2dab0ce4dcd00b441a95c1ce5e4b7ee948
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-04-01 18:25:41 -0400

    ran the file build on changes

--- src/pages/left/message/Posts.js
+++ src/pages/left/message/Posts.js
@@ -1,17 +1,9 @@
 import React, { Component } from "react";
-// import Spinner from "../../../components/spinner/spinner";
-// import Button from '../components/button/button'
 import UpdatePost from "../../../reducers/post/updatePost";
-// import Portal from '../../../../reducers/portal'
 
 //component on props child post
 class Posts extends Component {
   render() {
-    // if (data.length <= 0) {
-    //   // console.log(data.length)
-    //   // return <div id="Spinner"><Icon.FiLoader /></div>
-    //   return <Spinner />;
-    // }
     return (
       <>
         <div className="Posts" id="list-avatars">
