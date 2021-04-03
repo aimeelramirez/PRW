a28357e595704912aba56b8cf24047ae7411212b
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-04-03 17:17:40 -0400

    variables

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import Header from "./pages/header/header";
 // import Ads from './pages/right/ads';
 import Navigation from "./pages/left/navigation";
-
+import LogoHeader from './assets/logo-header'
 function App() {
   return (
     <>
+      <LogoHeader />
       <Header />
       <Navigation />
     </>
---
+++ src/assets/logo-header.js
@@ -0,0 +1,8 @@
+const LogoHeader = () => {
+    return (<>
+        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350.83 81.53"><defs>
+        </defs><text class="cls-1" transform="translate(0 53.64)">aimeelynn.dev</text></svg>
+    </>)
+}
+
+export default LogoHeader
