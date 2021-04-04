2cb5dfebd7efcd7fc66c2fa9eac372c41acf4824
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-04-03 21:44:51 -0400

    svg onanimation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import "./App.css";
 import Header from "./pages/header/header";
 // import Ads from './pages/right/ads';
 import Navigation from "./pages/left/navigation";
+import anime from 'animejs/lib/anime.es.js';
+
+const svgPath = document.querySelectorAll('.cls');
+
+const svgText = anime({
+  targets: svgPath,
+  loop: true,
+  direction: 'alternate',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 700,
+  delay: (el, i) => { return i * 500 }
+});
 function App() {
   return (
     <div className="App">
